3bb823270edd6b89fa5fef781575a9e5b88e712d
Victor A. Seixas <57509185+Victors3ixas@users.noreply.github.com>
2021-10-01 22:48:12 -0300

    Add files via upload

--- js/scroll_btn.js
+++ js/scroll_btn.js
@@ -1,8 +1,8 @@
-let scroll_btn= document.getElementById('scroll_btn').style;
+let scroll_btn= document.querySelector('.scroll_btn').style;
 
 window.addEventListener('scroll', function() {
     let btnY = window.pageYOffset;
 
     scroll_btn.pointerEvents = btnY < 500 ? "none" : "auto";
-    scroll_btn.opacity = btnY < 500 ? "0" : "0.75";
+    scroll_btn.opacity = btnY < 500 ? "0" : "1";
 })
