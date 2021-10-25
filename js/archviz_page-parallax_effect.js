6df01f7299e335fafe04cf4c537908765771badf
Victor A. Seixas <57509185+Victors3ixas@users.noreply.github.com>
2021-10-25 15:16:22 -0300

    Delete archviz_page-parallax_effect.js

--- js/archviz_page-parallax_effect.js
+++
@@ -1,7 +0,0 @@
-let section_content = document.getElementById('section_content').style;
-
-window.addEventListener('scroll', function() {
-    let scrollY = window.scrollY * 0.003;
-
-    section_content.opacity = 1 - scrollY;
-})
