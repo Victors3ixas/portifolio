521f66b67257afe074bb7180c05a28d6440c1d94
Victor A. Seixas <57509185+Victors3ixas@users.noreply.github.com>
2021-10-25 15:16:41 -0300

    Delete scroll_btn.js

--- js/scroll_btn.js
+++
@@ -1,8 +0,0 @@
-let scroll_btn= document.querySelector('.scroll_btn').style;
-
-window.addEventListener('scroll', function() {
-    let btnY = window.pageYOffset;
-
-    scroll_btn.pointerEvents = btnY < 500 ? "none" : "auto";
-    scroll_btn.opacity = btnY < 500 ? "0" : "1";
-})
